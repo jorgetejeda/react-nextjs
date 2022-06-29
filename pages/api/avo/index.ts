b1efcf4e43a9db27fd9e1959ffbfd7b0c100af28
Jorge de Jesus Tejeda Bello <jtejeda@jorges-mini-2.lan>
2022-06-29 17:28:26 -0400

    config our api

---
+++ pages/api/avo/index.ts
@@ -0,0 +1,12 @@
+import Database from '@database';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const allAvos = async (request: NextApiRequest, response: NextApiResponse<TProduct[]>) => {
+  const db = new Database();
+  const allEntries: TProduct[] = await db.getAll();
+  response.statusCode = 200
+  response.setHeader('Content-Type', 'application/json')
+  response.end(JSON.stringify({ data: allEntries }))
+}
+
+export default allAvos
