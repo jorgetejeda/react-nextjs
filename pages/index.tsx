01b78d8866fd0bce5d3ce49a7452c6c528d50870
Jorge de Jesus Tejeda Bello <jtejeda@jorges-mini-2.lan>
2022-06-30 17:20:24 -0400

    example of server side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next/types';
 import Link from 'next/link';
 
-const Home = () => {
-  const [productList, setProductList] = useState<TProduct[]>([]);
+// SSR
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const response = await fetch('http://localhost:3000/api/avo');
+  const { data: productList }: TAPIAvoResponse = await response.json();
+
+  return {
+    props: {
+      productList,
+    },
+  };
+};
+
+const Home = ({ productList }: { productList: TProduct[] }) => {
+  // const [productList, setProductList] = useState<TProduct[]>([]);
 
   //clienet side rendering (CRS)
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then((reponse) => reponse.json())
-      .then(({ data, length }) => {
-        setProductList(data);
-      })
-      .catch((error) => console.error(error.message));
-  }, []);
+  // useEffect(() => {
+  //   window
+  //     .fetch('/api/avo')
+  //     .then((reponse) => reponse.json())
+  //     .then(({ data, length }) => {
+  //       setProductList(data);
+  //     })
+  //     .catch((error) => console.error(error.message));
+  // }, []);
 
   return (
     <div>
       <h1>Products</h1>
-      {productList.map((product) => (
+      {productList?.map((product) => (
         <div key={product.id}>
           <Link href={`/product/${product.id}`}>
             <a>{product.name}</a>
