916ed99a974a9205a0f4f0848f2460f09f2c8c63
Jorge de Jesus Tejeda Bello <jtejeda@jorges-mini-2.lan>
2022-07-01 14:43:30 -0400

    create static server side

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,24 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import { GetStaticProps, GetStaticPaths } from 'next';
 
-const ProductItem = () => {
-  const {
-    query: { id },
-  } = useRouter();
-  const [product, setProduct] = useState<TProduct>();
+// Dynamic static generation: DSG
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch('https://platzi-avo.vercel.app/api/avo');
+  const { data: productList }: TAPIAvoResponse = await response.json();
 
-  useEffect(() => {
-    const fecthData = async () => {
-      const response = await fetch(`/api/avo/${id}`)
-      const data = await response.json()
-      setProduct(data)
-    }
-    id && fecthData()
-  }, [id]);
+  const paths = productList.map(({ id }) => ({
+    params: {
+      id,
+    },
+  }));
+
+  return {
+    // paths: [{ params: { id: ... } }],
+    paths,
+    fallback: false,
+  };
+};
+
+// pagina dinamica -
+// buil time - nextjs - antemano, necesita saber las paginas que se generaran
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params?.id as string;
+  const response = await fetch(`https://platzi-avo.vercel.app/api/avo/${id}`);
+  const data: TProduct = await response.json();
+  return {
+    props: {
+      product: data,
+    },
+  };
+};
+
+const ProductItem = ({ product }: { product: TProduct }) => {
+  // const {
+  //   query: { id },
+  // } = useRouter();
+  // const [product, setProduct] = useState<TProduct>();
+
+  // // CSR
+  // useEffect(() => {
+  //   const fecthData = async () => {
+  //     const response = await fetch(`/api/avo/${id}`);
+  //     const data = await response.json();
+  //     setProduct(data);
+  //   };
+  //   id && fecthData();
+  // }, [id]);
 
   return (
     <>
-      <div>ProductItem: {id}</div>
+      <div>ProductItem: {product?.id}</div>
       <div>{product?.name}</div>
     </>
   );
