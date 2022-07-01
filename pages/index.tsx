c0b9d079259e18d4c2a763c878f5a4a1f7725b25
Jorge de Jesus Tejeda Bello <jtejeda@jorges-mini-2.lan>
2022-07-01 14:43:52 -0400

    add server side rendering on index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { GetServerSideProps } from 'next/types';
+import { GetServerSideProps, GetStaticProps } from 'next/types';
 import Link from 'next/link';
+
+// SSG - estatica
+// export const getStaticProps: GetStaticProps = async (ctx) => {
+//   const response = await fetch('https://platzi-avo.vercel.app/api/avo');
+//   const { data: productList }: TAPIAvoResponse = await response.json();
+
+//   return {
+//     props: {
+//       productList,
+//     },
+//   };
+// };
 
 // SSR
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
@@ -33,7 +45,9 @@ const Home = ({ productList }: { productList: TProduct[] }) => {
       <h1>Products</h1>
       {productList?.map((product) => (
         <div key={product.id}>
-          <Link href={`/product/${product.id}`}>
+          <Link 
+            href='/product/[id]'
+            as={`/avocado/${product.id}`}>
             <a>{product.name}</a>
           </Link>
         </div>
